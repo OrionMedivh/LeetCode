363162fbcd76ebb753a8cb3c044e8104fb509e65
OrionMedivh <medivhsteve@gmail.com>
2017-02-25 15:38:29 -0600

    update solution

--- src/HIndex.java
+++ src/HIndex.java
@@ -1,32 +1,34 @@
-import java.util.Arrays;
 
 public class HIndex {
+//	public int hIndex(int[] citations) {
+//		if (citations == null || citations.length == 0)
+//			return 0;
+//		Arrays.sort(citations);
+//		int n = citations.length;
+//		for (int i = 0; i < n; i++) {
+//			if (citations[i] >= n - i)
+//				return n - i;
+//		}
+//		return 0;
+//	} //array sort is O(NlogN)
+	
 	public int hIndex(int[] citations) {
-		// if( citations == null || citations.length == 0) return 0;
-		// Arrays.sort(citations);
-		// int n = citations.length;
-		// int[] store= new int[n+1];
-		// for(int i =0 ;i<n; i++){
-		// if(citations[i] > n )
-		// store[n]++;
-		// else
-		// store[citations[i]]++;
-		// }
-		//
-		// for(int i = n; i>0; i--){
-		// store[i-1]+=store[i];
-		// if(store[i] >=i) return i;
-		// }
-		// return 0;
-
-		if (citations == null || citations.length == 0)
-			return 0;
-		Arrays.sort(citations);
-		int n = citations.length;
-		for (int i = 0; i < n; i++) {
-			if (citations[i] >= n - i)
-				return n - i;
-		}
-		return 0;
-	}
+	    int n = citations.length;
+	    int[] buckets = new int[n+1];
+	    for(int c : citations) {
+	        if(c >= n) {
+	            buckets[n]++;
+	        } else {
+	            buckets[c]++;
+	        }
+	    }
+	    int count = 0;
+	    for(int i = n; i >= 0; i--) {
+	        count += buckets[i];
+	        if(count >= i) {
+	            return i;
+	        }
+	    }
+	    return 0;
+	} // Bucket sort! O(N)
 }
