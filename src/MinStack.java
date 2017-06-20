c52203b876b8015f9f80e80c959d7cbf9d8bb9eb
OrionMedivh <medivhsteve@gmail.com>
2017-06-19 22:22:51 -0500

    more questions

--- src/MinStack.java
+++ src/MinStack.java
@@ -1,40 +1,91 @@
 import java.util.Stack;
 
+/*
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+getMin() -- Retrieve the minimum element in the stack.
+Example:
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> Returns -3.
+minStack.pop();
+minStack.top();      --> Returns 0.
+minStack.getMin();   --> Returns -2.
+ */
+
 public class MinStack {
-private Stack<Integer> stack;
-private Stack<Integer> minStack;
-	
-	//Constructor
-	public MinStack(){
+	private Stack<Integer> stack;
+	private Stack<Integer> minStack;
+
+	/** initialize your data structure here. */
+	public MinStack() {
 		stack = new Stack<>();
 		minStack = new Stack<>();
 	}
-	
-	public void push(int x){
+
+	public void push(int x) {
 		stack.push(x);
-		if(minStack.isEmpty() || x <= minStack.peek()){
-			//we only push x into minStack when it's <= minStack.peek()
+		if (minStack.isEmpty() || x <= minStack.peek()) {
+			// we only push x into minStack when it's <= minStack.peek()
 			minStack.push(x);
 		}
 	}
-	
-	public void pop(){
-		if(stack.isEmpty()) {
-			return;
-		}
-		if(!minStack.empty() && minStack.peek().equals(stack.peek())){
+
+	public void pop() {
+		if (!minStack.empty() && minStack.peek().equals(stack.pop())) {
 			minStack.pop();
 		}
 	}
-	
-	public int top(){
+
+	public int top() {
 		return stack.peek();
 	}
-	
-	public int getMin(){
+
+	public int getMin() {
 		return minStack.peek();
 	}
-	
-	//5,4,1,3,2
-	//5,4,1,(1),(1)
 }
+
+// Requires more space, but more intuitive.
+class MinStack2 {
+	private Stack<Integer> stack;
+	private Stack<Integer> minStack;
+
+	/** initialize your data structure here. */
+	public MinStack2() {
+		stack = new Stack<>();
+		minStack = new Stack<>();
+	}
+
+	public void push(int x) {
+		stack.push(x);
+		minStack.push(Math.min(x, minStack.isEmpty() ? x : minStack.peek()));
+	}
+
+	public void pop() {
+		minStack.pop();
+		stack.pop();
+	}
+
+	public int top() {
+		return stack.peek();
+	}
+
+	public int getMin() {
+		return minStack.peek();
+	}
+}
+
+// Another way is to keep an 'min' as global integer, but need to be careful
+// about overflow
+
+/**
+ * Your MinStack object will be instantiated and called as such: MinStack obj =
+ * new MinStack(); obj.push(x); obj.pop(); int param_3 = obj.top(); int param_4
+ * = obj.getMin();
+ */
